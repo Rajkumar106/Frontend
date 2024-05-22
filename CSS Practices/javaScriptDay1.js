11948c3bc7c85cc0701bcbb7bcfbc3a5d2da5774
Rajkumar S <rajkumarsambath54@gmail.com>
2024-05-22 19:55:50 +0530

    Added all Frontend practices

---
+++ CSS Practices/javaScriptDay1.js
@@ -0,0 +1,13 @@
+var price=20;
+var product="Cap";
+var tax=15;
+console.log(product)
+var total=price+tax;
+console.log(total)
+
+var fruitName="Apple"
+var count=4
+var price=40
+var total =count * price
+console.log(fruitName)
+console.log(price)  
---
+++ JavaScript Practices/fuctionAdd.js
@@ -0,0 +1,26 @@
+function add(a,b){
+    console.log(a+b)
+}
+
+add(10,12)
+
+var factor= "Vijay"
+var fplayer= "Hardik"
+var fmovie= "Kaththi"
+
+function favourite(){
+    console.log("Favourite Actor:"+ factor);
+    console.log("Favourite Player:", fplayer);
+    console.log("Favourite Movie:", fmovie);
+}
+favourite()
+
+function area(length,breadth){
+    console.log(length*breadth);
+}
+area(40,10)
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(10,12));
